4a476d0a13b514f6de16f1dec17a9f0b63e3a646
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-31 15:05:29 -0800

    added redirect for when user is logged in. You now go to items list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import backgroundImage from './background.webp';
 import Header from './Components/Header/Header';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Auth from './Components/Auth/Auth';
-import Items from './Context/ItemsContext';
+import Items from './Components/Items/Items.js';
 import { useContext } from 'react';
 import { UserContext } from './Context/UserContext.js';
 
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useContext } from 'react';
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect, useParams } from 'react-router-dom';
+import { UserContext } from '../../Context/UserContext.js';
+import { authUser } from '../../services/auth.js';
 import './Auth.css';
 
 export default function Auth() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const { type } = useParams();
+  const { user, setUser } = useContext(UserContext);
 
-  const submitAuth = async () => {
-
+  if (user) {
+    return <Redirect to='/items'/>;
+  }
+  const submitAuth = async (e) => {
+    e.preventDefault();
+    try {
+      const user = authUser(email, password, type);
+      setUser(user);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -17,6 +31,15 @@ export default function Auth() {
         <div className='nav-heading'>To Do</div>
         <NavLink to='/auth/sign-in'>Sign in</NavLink>
         <NavLink to='/auth/sign-up'>Sign Up</NavLink>
+        <div className='form'>
+          <form onSubmit={submitAuth}>
+            <label>Email</label>
+            <input type='email' placeholder='me@me.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <label>Password</label>
+            <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button className='form-submit-btn'>Submit</button>
+          </form>
+        </div>
       </nav>
     </div>
   );
