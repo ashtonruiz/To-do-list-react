3e4664fd459f21ac54b84d4ec8b19aab8c00ed5f
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-31 13:02:28 -0800

    added some items stuff so that I could continue working on the redirects

---
+++ src/Components/Items/Items.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../../Context/UserContext.js';
+import { Redirect } from 'react-router-dom';
+
+export default function Items() {
+  const { user } = useContext(UserContext);
+  if (!user) {
+    return <Redirect to='/' />;
+  }
+  return (
+    <div>
+      <p>Task Item</p>
+    </div>
+  );
+}
